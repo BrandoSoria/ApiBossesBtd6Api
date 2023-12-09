61e7729caa336890a220fa5f889d3f800636fa9f
brandon <webarebrandy@gmail.com>
2023-12-08 22:34:24 -0600

    agregar favoritos y quitar favoritos

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000;
 const axios = require('axios');
 const db = require('./db/db'); // Ajusta la ruta según la estructura de tu proyecto
 
+
+
 app.get('/bosses', async (req, res) => {
   try {
     const response = await axios.get('https://data.ninjakiwi.com/btd6/bosses');
@@ -14,6 +16,48 @@ app.get('/bosses', async (req, res) => {
     res.json(bosses);
   } catch (error) {
     console.error('Error al obtener la información de los jefes:', error.message);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+
+
+app.use(express.json());
+
+app.post('/favoritos/agregar', async (req, res) => {
+  const { nombreJefe, imagen } = req.body;
+
+  try {
+    const result = await db.request()
+      .input('nombreJefe', db.NVarChar, nombreJefe)
+      .input('imagen', db.NVarChar, imagen)
+      .query(`
+        INSERT INTO JefesFavoritos (NombreJefe, Imagen)
+        VALUES (@nombreJefe, @imagen)
+      `);
+
+    res.status(201).json({ message: 'Jefe favorito agregado correctamente' });
+  } catch (error) {
+    console.error('Error al agregar jefe favorito:', error.message);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+
+//eliminar un jefe favorito si ya no me gusto
+
+app.delete('/favoritos/quitar/:nombreJefe', async (req, res) => {
+  const nombreJefe = req.params.nombreJefe;
+
+  try {
+    const result = await db.request()
+      .input('nombreJefe', db.NVarChar, nombreJefe)
+      .query(`
+        DELETE FROM JefesFavoritos
+        WHERE NombreJefe = @nombreJefe
+      `);
+
+    res.json({ message: 'Jefe favorito eliminado correctamente' });
+  } catch (error) {
+    console.error('Error al quitar jefe favorito:', error.message);
     res.status(500).send('Error interno del servidor');
   }
 });
