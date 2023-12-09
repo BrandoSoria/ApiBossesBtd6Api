8ac1570c67742969eeca043f91c4018d63d092d3
brandon <webarebrandy@gmail.com>
2023-12-08 22:42:24 -0600

    agg y eliminar fav co  logica para obtener informacio favs de bd y de la api externa

--- app.js
+++ app.js
@@ -4,29 +4,37 @@ const port = 3000;
 const axios = require('axios');
 const db = require('./db/db'); // Ajusta la ruta según la estructura de tu proyecto
 
-
-
+// Ruta para obtener información de los jefes de la API de Ninja Kiwi y jefes favoritos
 app.get('/bosses', async (req, res) => {
   try {
+    // Obtener información de los jefes de la API de Ninja Kiwi
     const response = await axios.get('https://data.ninjakiwi.com/btd6/bosses');
     const bosses = response.data;
 
-    // Consulta para obtener los jefes favoritos desde la base de datos (aún no implementada)
+    // Obtener jefes favoritos desde la base de datos
+    const result = await db.request().query('SELECT * FROM JefesFavoritos');
+    const jefesFavoritos = result.recordset;
 
-    res.json(bosses);
+    // Combinar la información de jefes de la API y jefes favoritos
+    const todosLosJefes = [...bosses, ...jefesFavoritos];
+
+    // Enviar la información de todos los jefes
+    res.json(todosLosJefes);
   } catch (error) {
     console.error('Error al obtener la información de los jefes:', error.message);
     res.status(500).send('Error interno del servidor');
   }
 });
 
-
+// Middleware para parsear el cuerpo de las solicitudes como JSON
 app.use(express.json());
 
+// Ruta para agregar un jefe favorito
 app.post('/favoritos/agregar', async (req, res) => {
   const { nombreJefe, imagen } = req.body;
 
   try {
+    // Insertar el jefe favorito en la base de datos
     const result = await db.request()
       .input('nombreJefe', db.NVarChar, nombreJefe)
       .input('imagen', db.NVarChar, imagen)
@@ -42,12 +50,12 @@ app.post('/favoritos/agregar', async (req, res) => {
   }
 });
 
-//eliminar un jefe favorito si ya no me gusto
-
+// Ruta para quitar un jefe favorito
 app.delete('/favoritos/quitar/:nombreJefe', async (req, res) => {
   const nombreJefe = req.params.nombreJefe;
 
   try {
+    // Eliminar el jefe favorito de la base de datos
     const result = await db.request()
       .input('nombreJefe', db.NVarChar, nombreJefe)
       .query(`
@@ -62,8 +70,7 @@ app.delete('/favoritos/quitar/:nombreJefe', async (req, res) => {
   }
 });
 
-// Agrega la ruta para manejar la adición de jefes favoritos aquí (consulta a la base de datos)
-
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`El servidor está escuchando en http://localhost:${port}`);
 });
