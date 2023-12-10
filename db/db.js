972aea7500f7afd06276dc146605bf55b88f143b
brandon <webarebrandy@gmail.com>
2023-12-09 23:18:46 -0600

    variables configurada correctamente

--- db/db.js
+++ db/db.js
@@ -2,10 +2,10 @@
 const sql = require('mssql');
 
 const config = {
-  user: 'brandon',
-  password: 'Brandon?',
-  server: 'DESKTOP-HQNQOQO',
-  database: 'Boss',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
   options: {
     encrypt: false,
   },
