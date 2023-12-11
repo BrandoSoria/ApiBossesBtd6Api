e56447515010ad0790719b9fd26083cddbebb956
brandon <webarebrandy@gmail.com>
2023-12-11 13:31:01 -0600

    solucion 1

--- middleware.js
+++ middleware.js
@@ -1,15 +1,27 @@
+// middleware.js
+
 const jwt = require('jsonwebtoken');
+const secretKey = 'tu_secreto_jwt';
 
 const authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ error: 'Acceso no autorizado' });
+  const token = req.headers['authorization'];
 
-  jwt.verify(token, 'WD3Y26STVOBXSS4J47EXZMRKMKYOSB3X', (err, user) => {
-    if (err) return res.status(403).json({ error: 'Token inválido' });
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
 
+  jwt.verify(token.split(' ')[1], secretKey, (err, user) => {
+    if (err) {
+      console.error('Error al verificar el token:', err.message);
+      return res.status(403).json({ error: 'Token no válido', details: err.message });
+    }
+
+    console.log('Usuario autenticado:', user);
     req.user = user;
     next();
   });
 };
 
-module.exports = { authenticateToken };
+module.exports = {
+  authenticateToken,
+};
