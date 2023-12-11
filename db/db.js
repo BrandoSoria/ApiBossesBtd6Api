add2a3169812d516aa1fffb57d89c619dc520014
brandon <webarebrandy@gmail.com>
2023-12-10 21:12:24 -0600

    a

--- db/db.js
+++ db/db.js
@@ -1,19 +1,21 @@
-
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Puerto predeterminado de MySQL
+  port: process.env.DB_PORT || 3306, // Ajusta el puerto según la configuración de tu base de datos MySQL
 };
 
 async function connectToDatabase() {
   try {
     const connection = await mysql.createConnection(config);
     console.log('Conexión exitosa a la base de datos');
-    connection.release(); // Liberar la conexión después de su uso
+    return connection;
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error.message);
+    throw error;
   }
 }
 
