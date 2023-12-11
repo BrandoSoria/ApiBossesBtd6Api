173b68511100a92ac1baacb51b53e7a865244ab1
brandon <webarebrandy@gmail.com>
2023-12-10 21:44:13 -0600

    resolicion 4

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const axios = require('axios');
-const mysql = require('mysql2/promise');
 const db = require('./db/db');
 
 app.use(express.json());
@@ -11,13 +10,11 @@ app.use(express.json());
 app.get('/bosses', async (req, res) => {
   try {
     const response = await axios.get('https://data.ninjakiwi.com/btd6/bosses');
-    const bosses = response.data;
+    const bosses = response.data.body;
 
-    const connection = await db.connectToDatabase();
-    const [rows] = await connection.query('SELECT * FROM JefesFavoritos');
-    connection.end(); // Cerrar la conexión después de usarla
+    const [jefesFavoritos] = await db.query('SELECT * FROM JefesFavoritos');
 
-    res.json({ bosses, jefesFavoritos: rows });
+    res.json({ bosses, jefesFavoritos });
   } catch (error) {
     console.error('Error al obtener la información de los jefes:', error.message);
     res.status(500).json({ error: 'Error al obtener la información de los jefes', details: error.message });
@@ -28,25 +25,14 @@ app.post('/favoritos/agregar', async (req, res) => {
   const { nombreJefe, imagen } = req.body;
 
   try {
-    // Verificar que se haya proporcionado nombreJefe
     if (!nombreJefe) {
       return res.status(400).json({ error: 'Se requiere nombreJefe' });
     }
 
-    const connection = await db.connectToDatabase();
-    const [result] = await connection.query(
-      'INSERT INTO JefesFavoritos (NombreJefe, Imagen) VALUES (?, ?)',
-      [nombreJefe, imagen]
-    );
-    connection.end(); // Cerrar la conexión después de usarla
+    const [result] = await db.query('INSERT INTO JefesFavoritos (NombreJefe, Imagen) VALUES (?, ?)', [nombreJefe, imagen]);
 
-    const [jefesAgregados] = await connection.query(
-      'SELECT * FROM JefesFavoritos WHERE NombreJefe = ?',
-      [nombreJefe]
-    );
-
-    if (jefesAgregados.length > 0) {
-      const jefeAgregado = jefesAgregados[0];
+    if (result.affectedRows > 0) {
+      const jefeAgregado = { Id: result.insertId, NombreJefe: nombreJefe, Imagen: imagen };
       res.status(201).json({ success: true, message: 'Jefe favorito agregado correctamente', jefeAgregado });
     } else {
       res.status(500).json({ error: 'Error al agregar jefe favorito', details: 'No se pudo obtener el jefe recién agregado' });
@@ -57,15 +43,17 @@ app.post('/favoritos/agregar', async (req, res) => {
   }
 });
 
-app.delete('/favoritos/quitar/:nombreJefe', async (req, res) => {
-  const nombreJefe = req.params.nombreJefe;
+app.delete('/favoritos/quitar/:id', async (req, res) => {
+  const idJefe = req.params.id;
 
   try {
-    const connection = await db.connectToDatabase();
-    await connection.query('DELETE FROM JefesFavoritos WHERE NombreJefe = ?', [nombreJefe]);
-    connection.end(); // Cerrar la conexión después de usarla
+    const [result] = await db.query('DELETE FROM JefesFavoritos WHERE Id = ?', [idJefe]);
 
-    res.json({ success: true, message: 'Jefe favorito eliminado correctamente' });
+    if (result.affectedRows > 0) {
+      res.json({ success: true, message: 'Jefe favorito eliminado correctamente' });
+    } else {
+      res.status(404).json({ error: 'Jefe favorito no encontrado', details: 'No se pudo encontrar y eliminar el jefe favorito' });
+    }
   } catch (error) {
     console.error('Error al quitar jefe favorito:', error.message);
     res.status(500).json({ error: 'Error al quitar jefe favorito', details: error.message });
--- db/db.js
+++ db/db.js
@@ -1,26 +1,21 @@
-const mysql = require('mysql2/promise'); // Uso de 'mysql2/promise' para trabajar con Promesas
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const config = {
-  host: process.env.DB_SERVER,
+// Configuración de la conexión a la base de datos
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Puerto típico de MySQL
-};
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-// Función para conectar a la base de datos
-async function connectToDatabase() {
-  try {
-    const connection = await mysql.createConnection(config);
-    console.log('Conexión exitosa a la base de datos');
-    return connection;
-  } catch (err) {
-    console.error('Error al conectar a la base de datos:', err.message);
-    throw err;
-  }
-}
-
-// Exportar la función de conexión
 module.exports = {
-  connectToDatabase,
+  query: async (sql, values) => {
+    const [results] = await pool.query(sql, values);
+    return results;
+  }
 };
